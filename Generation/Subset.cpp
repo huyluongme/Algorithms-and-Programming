755c015e528beb6a9e81f8a83dbbc6a4e60da358
Duc Huy (Hui) <48280785+huyluongme@users.noreply.github.com>
2021-12-12 21:29:39 +0700

    Update Subset.cpp

--- Generation/Subset.cpp
+++ Generation/Subset.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
+int main() {
 	int n, k, x[30], id;
 	cin >> n >> k;
-	if (k == 0)
-		cout << "Empty set";
-	else if (k > 0) {
-		for (int i = 0; i < k; i++) x[i] = i + 1;
+	
+	// Xây dựng cấu hình đầu tiên
+	for (int i = 0; i < k; i++) x[i] = i + 1;
 
-		do {
-			cout << "{";
-			for (int i = 0; i < k - 1; i++) cout << x[i] << ", ";
-			cout << x[k - 1] << "}\n";
+	do {
+		// In cấu hình hiện tại
+		cout << "{";
+		for (int i = 0; i < k - 1; i++) cout << x[i] << ", ";
+		cout << x[k - 1] << "}" << endl;
 
-			id = k - 1;
-			while (id >= 0 && x[id] == n - k + id + 1) id--;
+		// Gán id bằng phần tử tứ k - 1 và lùi id trong khi id >= 0 và giá trị phần tử tại vị trí id bằng với giới hạn trên
+		id = k - 1;
+		while (id >= 0 && x[id] == n - k + id + 1) id--;
 
-			if (id >= 0) {
-				x[id]++;
-				for (int i = id + 1; i < k; i++) x[i] = x[i - 1] + 1;
-			}
+		// Xây dựng cấu hình kế tiếp
+		if (id >= 0) {
+			x[id]++;
+			for (int i = id + 1; i < k; i++) x[i] = x[i - 1] + 1;
+		}
+	} while (id >= 0);
 
-		} while (id >= 0);
-	}
 	return 0;
 }
